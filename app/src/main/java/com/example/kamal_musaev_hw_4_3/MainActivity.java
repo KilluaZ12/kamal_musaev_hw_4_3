62d0a8b2e0db18fe387a8345057a836c39ff64fb
KilluaZ12 <kamalmusaev.12.92@gmail.com>
2023-05-22 17:04:51 +0600

    ispravil summu

--- app/src/main/java/com/example/kamal_musaev_hw_4_3/MainActivity.java
+++ app/src/main/java/com/example/kamal_musaev_hw_4_3/MainActivity.java
@@ -31,22 +31,22 @@ public class MainActivity extends AppCompatActivity implements OnItemClick{
         products.clear();
         products.add(new Product("Cucumber",
                 "Cucumbers are commonly mistaken for vegetables. But in fact they are fruits, specifically berries.",
-                R.drawable.cucumber, "150", R.color.green));
+                R.drawable.cucumber, "150 som/kg", R.color.green));
         products.add(new Product("Watermelon",
                 "It has an oval or spherical shape and a dark green and smooth rind, sometimes showing irregular areas of a pale green colour. ",
-                R.drawable.watermelon, "200", R.color.green));
+                R.drawable.watermelon, "200 som/kg", R.color.green));
         products.add(new Product("Tomato",
                 "They are usually red, scarlet, or yellow, though green and purple varieties do exist, and they vary in shape from almost spherical to oval and elongate to pear-shaped.",
-                R.drawable.tomato, "120", R.color.red));
+                R.drawable.tomato, "120 som/kg", R.color.red));
         products.add(new Product("Strawberry",
                 "A strawberry is both a low-growing, flowering plant and also the name of the fruit that it produces. ",
-                R.drawable.strawberry, "350", R.color.red));
+                R.drawable.strawberry, "350 som/kg", R.color.red));
         products.add(new Product("Orange",
                 "Orange trees have dark green shiny leaves and small white flowers with five petals. The flowers smell very sweet which attracts many bees.",
-                R.drawable.orange, "180", R.color.orange));
+                R.drawable.orange, "180 som/kg", R.color.orange));
         products.add(new Product("Carrot",
                 "Carrot is a biennial, belonging to the family Apiaceae, and is an important vegetable for its fleshy edible, colorful roots.",
-                R.drawable.carrot, "45", R.color.orange));
+                R.drawable.carrot, "45 som/kg", R.color.orange));
 
     }
 
